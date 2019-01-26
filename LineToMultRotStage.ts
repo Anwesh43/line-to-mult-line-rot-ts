af7767baadb582129f3cc9be393a4c346f8916a6
Anwesh43 <anweshthecool0@gmail.com>
2019-01-26 21:46:17 +0530

    adding constants

---
+++ LineToMultRotStage.ts
@@ -0,0 +1,8 @@
+const nodes : number = 5
+const lines : number = 4
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 2.8
+const foreColor : string = "#673AB7"
+const backColor : string = "#BDBDBD"
