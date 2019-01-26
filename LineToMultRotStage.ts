b9bdc7ee159a856d0340c7a49fbe86c1800298c5
Anwesh43 <anweshthecool0@gmail.com>
2019-01-26 23:11:21 +0530

    adding drawLTMRNode method

--- LineToMultRotStage.ts
+++ LineToMultRotStage.ts
@@ -6,6 +6,8 @@ const strokeFactor : number = 90
 const sizeFactor : number = 2.8
 const foreColor : string = "#673AB7"
 const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth
+const h : number = window.innerHeight
 
 const maxScale : Function = (scale : number, i : number, n : number) : number => {
   return Math.max(0, scale - i / n)
@@ -20,4 +22,35 @@ const mirrorValue : Function = (scale : number, a : number, b : number) : number
 }
 const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
     return mirrorValue(scale, a, b) * dir * scGap
+}
+
+const drawVerToHorLine = (context : CanvasRenderingContext2D, i : number, size : number, scale : number) => {
+    context.save()
+    context.translate(0, size * i)
+    context.beginPath()
+    context.moveTo(0, size * scale)
+    context.lineTo(0, size)
+    context.stroke()
+    context.beginPath()
+    context.moveTo(0, size)
+    context.lineTo(size * scale, size)
+    context.stroke()
+    context.restore()
+}
+const drawLTMRNode : Function = (context : CanvasRenderingContext2D, i : number, scale : number) => {
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    const gap : number = w / (nodes + 1)
+    const size : number = gap / sizeFactor
+    const ySize : number = size / lines
+    context.strokeStyle = foreColor
+    context.lineCap = 'round'
+    context.lineWidth = Math.min(w, h) / strokeFactor
+    context.save()
+    context.translate(gap * (i + 1), h / 2)
+    context.rotate(Math.PI/2 * ( 1 - sc2))
+    for (var j = 0; j < lines; j++) {
+        drawVerToHorLine(context, j, ySize, divideScale(sc2, j, lines))
+    }
+    context.restore()
 }
