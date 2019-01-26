8a8b0b6d1b75a7f6171f57dd57871b1ce7abcab6
Anwesh43 <anweshthecool0@gmail.com>
2019-01-26 23:35:44 +0530

    adding LTMRNode class

--- LineToMultRotStage.ts
+++ LineToMultRotStage.ts
@@ -126,3 +126,48 @@ class Animator {
         }
     }
 }
+
+class LTMRNode {
+    state : State = new State()
+    prev : LTMRNode
+    next : LTMRNode
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new LTMRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawLTMRNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LTMRNode {
+        var curr : LTMRNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this 
+    }
+
+}
