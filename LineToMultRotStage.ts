dca04df8bbfc7f351863f673d0ed46d0f6a8a6f0
Anwesh43 <anweshthecool0@gmail.com>
2019-01-26 23:42:27 +0530

    adding LineToMultRotStage class

--- LineToMultRotStage.ts
+++ LineToMultRotStage.ts
@@ -167,7 +167,29 @@ class LTMRNode {
             return curr
         }
         cb()
-        return this 
+        return this
+    }
+}
+
+class LineToMultRot {
+    root : LTMRNode = new LTMRNode(0)
+    curr : LTMRNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
     }
 
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
 }
